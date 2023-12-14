af9267741616e4bb9d78abcece7b9e602613334b
savindu19990928 <devriousteam@gmail.com>
2023-12-15 01:50:46 +0530

    Update socketioService.js

--- src/services/socketioService.js
+++ src/services/socketioService.js
@@ -7,13 +7,13 @@ let io;
 const setupSocketIO = (server) => {
   io = socketIO(server);
 
-  // Periodically (Every 5 seconds) emit cryptocurrency price updates
+  // Periodically (Every 5 min) emit cryptocurrency price updates
   setInterval(async () => {
     try {
       const prices = await getPrices();
       emitPriceUpdate(prices);
     } catch (error) {}
-  }, 5000);
+  }, 300000);
 };
 
 // Emit a price update to all connected clients
